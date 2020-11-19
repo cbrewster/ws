7afe2fabeae5f3b260e5f07e5a17f253797bb753
Connor Brewster <cbrewster@hey.com>
2020-11-18 22:03:37 -0600

    Send and receive messages... sort of

--- examples/server.rs
+++ examples/server.rs
@@ -1,15 +1,20 @@
-use hyper::{
-    service::{make_service_fn, service_fn},
-    Server,
-};
+use hyper::{service::make_service_fn, Server};
 use std::{convert::Infallible, net::SocketAddr};
-use ws::web_socket;
+use ws::{Message, WebSocketHandler};
 
 #[tokio::main]
 async fn main() {
     let addr = SocketAddr::from(([127, 0, 0, 1], 9001));
     let server = Server::bind(&addr).serve(make_service_fn(|_| async {
-        Ok::<_, Infallible>(service_fn(web_socket))
+        Ok::<_, Infallible>(WebSocketHandler::new(|mut ws| async move {
+            while let Some(msg) = ws.recv().await {
+                dbg!(&msg);
+                dbg!(ws.send(msg).await.ok());
+                ws.send(Message::Binary(vec![0x1, 0x2, 0x3, 0x4]))
+                    .await
+                    .ok();
+            }
+        }))
     }));
 
     if let Err(e) = server.await {
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,40 @@
 #[macro_use]
 extern crate bitflags;
 
-use std::convert::Infallible;
+use std::{convert::Infallible, future::Future, pin::Pin, task::Poll};
 
 use anyhow::Result;
 use byteorder::ReadBytesExt;
 use http::{header::CONNECTION, header::UPGRADE, Method, Request, Response, StatusCode};
-use hyper::{upgrade::Upgraded, Body};
+use hyper::{service::Service, upgrade::Upgraded, Body};
 use sha1::Digest;
 use thiserror::Error;
-use tokio::io::{AsyncRead, AsyncReadExt, AsyncWriteExt};
+use tokio::{
+    io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt, ReadHalf, WriteHalf},
+    sync::mpsc::{error::SendError, Receiver, Sender},
+};
+
+#[derive(Debug)]
+pub enum Message {
+    Text(String),
+    Binary(Vec<u8>),
+    Close,
+}
+
+pub struct WebSocket {
+    sender: Sender<Message>,
+    receiver: Receiver<Message>,
+}
+
+impl WebSocket {
+    pub async fn recv(&mut self) -> Option<Message> {
+        self.receiver.recv().await
+    }
+
+    pub async fn send(&mut self, msg: Message) -> Result<(), SendError<Message>> {
+        self.sender.send(msg).await
+    }
+}
 
 #[derive(Error, Debug)]
 pub enum WsError {
@@ -30,7 +55,7 @@ bitflags! {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 enum Opcode {
     Continuation,
     Text,
@@ -54,6 +79,21 @@ impl From<u8> for Opcode {
             0xA => Opcode::Pong,
             value @ 0xB..=0xF => Opcode::Control(value),
             _ => unreachable!(),
+        }
+    }
+}
+
+impl Into<u8> for Opcode {
+    fn into(self) -> u8 {
+        match self {
+            Opcode::Continuation => 0x0,
+            Opcode::Text => 0x1,
+            Opcode::Binary => 0x2,
+            Opcode::NonControl(v) => v,
+            Opcode::ConnectionClose => 0x8,
+            Opcode::Ping => 0x9,
+            Opcode::Pong => 0xA,
+            Opcode::Control(v) => v,
         }
     }
 }
@@ -138,11 +178,63 @@ impl Frame {
 
         Ok(Frame {
             opcode,
-            flags: Flags::empty(),
+            flags,
             payload_length,
             extension: vec![],
             application,
         })
+    }
+
+    async fn write<W: AsyncWrite + Unpin>(&self, writer: &mut W) -> Result<(), std::io::Error> {
+        let mut byte = 0u8;
+
+        // First Byte:
+        // | FIN | RSV1 | RSV2 | RSV3 | OPCODE (4) |
+
+        if self.flags.contains(Flags::FIN) {
+            byte |= 0x80;
+        }
+
+        if self.flags.contains(Flags::RSV1) {
+            byte |= 0x40;
+        }
+
+        if self.flags.contains(Flags::RSV2) {
+            byte |= 0x20;
+        }
+
+        if self.flags.contains(Flags::RSV3) {
+            byte |= 0x10;
+        }
+
+        let opcode: u8 = self.opcode.into();
+        // Mask it for sanity.
+        byte |= opcode & 0x0F;
+
+        writer.write(&[byte]).await?;
+
+        // Always mask
+        byte = 0x80;
+
+        if self.payload_length < 126 {
+            byte |= self.payload_length as u8;
+        } else {
+            todo!()
+        }
+
+        writer.write(&[byte]).await?;
+
+        // TODO: lol rand gen
+        let mask = [0xFF, 0x00, 0xFF, 0x00];
+
+        writer.write(&mask).await?;
+
+        // TODO: Maybe buffer this up into bigger chunks before writing?
+        for (i, byte) in self.application.iter().enumerate() {
+            writer.write(&[byte ^ mask[i % 4]]).await?;
+        }
+
+        Ok(())
     }
 }
 
@@ -153,52 +245,227 @@ fn bad_request() -> Response<Body> {
         .unwrap()
 }
 
-async fn handle_upgraded(mut upgraded: Upgraded) -> Result<()> {
-    println!("We're doing it!");
-    loop {
-        let frame = Frame::read(&mut upgraded).await?;
-        // TODO: Check flags and return error if something is up!
+struct WebSocketWriter {
+    writer: WriteHalf<Upgraded>,
+}
 
-        println!("Got frame {:?}", frame);
+impl WebSocketWriter {
+    async fn write_message(&mut self, msg: Message) -> Result<(), WsError> {
+        // For now lets deliver this is payload chunks of <= 125 bytes
+        let (opcode, bytes) = match &msg {
+            Message::Close => todo!(),
+            Message::Binary(binary) => (Opcode::Binary, binary.as_slice()),
+            Message::Text(text) => (Opcode::Text, text.as_bytes()),
+        };
+
+        let total_chunks = bytes.len() / 125;
+
+        for (i, chunk) in bytes.chunks(125).enumerate() {
+            let mut flags = Flags::empty();
+            if i == total_chunks {
+                flags |= Flags::FIN;
+            }
+            let frame = Frame {
+                flags,
+                opcode: if i == 0 { opcode } else { Opcode::Continuation },
+                payload_length: chunk.len() as u64,
+                extension: vec![],
+                application: chunk.into(),
+            };
+            frame.write(&mut self.writer).await?;
+        }
+
+        Ok(())
     }
 }
 
-pub async fn web_socket(req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    if let Err(e) = handshake(&req) {
-        eprintln!("Handshake error: {}", e);
-        return Ok(bad_request());
-    }
+struct WebSocketReader {
+    reader: ReadHalf<Upgraded>,
+}
 
-    let accept_key = match compute_accept_key(&req) {
-        Ok(key) => key,
-        Err(e) => {
-            eprintln!("Computing Sec-WebSocket-Accept: {}", e);
-            return Ok(bad_request());
-        }
-    };
+impl WebSocketReader {
+    async fn read_message(&mut self) -> Result<Message, WsError> {
+        let mut application_buffer = vec![];
+        let mut current_op = None;
 
-    println!("Lookin good!");
+        loop {
+            let mut frame = Frame::read(&mut self.reader).await?;
 
-    tokio::spawn(async move {
-        match req.into_body().on_upgrade().await {
-            Ok(upgraded) => {
-                if let Err(e) = handle_upgraded(upgraded).await {
-                    eprintln!("WebSocket error: {}", e);
+            // TODO: Handle these cases
+            match frame.opcode {
+                Opcode::NonControl(_)
+                | Opcode::ConnectionClose
+                | Opcode::Ping
+                | Opcode::Pong
+                | Opcode::Control(_) => todo!(),
+                _ => {}
+            };
+
+            // We should either be starting a new message or get continuations.
+            match (current_op, frame.opcode) {
+                (None, op) => {
+                    current_op = Some(op);
                 }
+                (Some(_), Opcode::Continuation) => {}
+                _ => return Err(WsError::ProtocolError("Unexpected opcode".into())),
             }
-            Err(e) => eprintln!("Failed to upgrade: {}", e),
+            application_buffer.append(&mut frame.application);
+
+            // If this was the last frame, gather the bits and deliver the message to the application.
+            if frame.flags.contains(Flags::FIN) {
+                let op = match current_op {
+                    None => return Err(WsError::ProtocolError("Expected op".into())),
+                    Some(op) => op,
+                };
+
+                match op {
+                    Opcode::Binary => return Ok(Message::Binary(application_buffer)),
+                    Opcode::Text => {
+                        return Ok(Message::Text(
+                            String::from_utf8(application_buffer).map_err(|_| {
+                                WsError::ProtocolError("content not valid utf8".into())
+                            })?,
+                        ))
+                    }
+                    _ => return Err(WsError::ProtocolError("Unexpected opcode".into())),
+                };
+            }
+        }
+    }
+}
+
+async fn handle_upgraded(
+    upgraded: Upgraded,
+    mut receiver: Receiver<Message>,
+    mut sender: Sender<Message>,
+) -> Result<(), WsError> {
+    let (reader, writer) = tokio::io::split(upgraded);
+
+    tokio::task::spawn(async move {
+        let mut writer = WebSocketWriter { writer };
+        while let Some(msg) = receiver.recv().await {
+            if let Err(err) = writer.write_message(msg).await {
+                eprintln!("Write message error: {}", err);
+            }
         }
     });
 
-    let res = Response::builder()
-        .status(StatusCode::SWITCHING_PROTOCOLS)
-        .header(UPGRADE, "websocket")
-        .header(CONNECTION, "Upgrade")
-        .header("Sec-WebSocket-Accept", accept_key)
-        .body(Body::empty())
-        .expect("failed to build response");
+    tokio::task::spawn(async move {
+        let mut reader = WebSocketReader { reader };
+        loop {
+            let msg = match reader.read_message().await {
+                Ok(frame) => frame,
+                Err(WsError::IoError(err)) if err.kind() == std::io::ErrorKind::UnexpectedEof => {
+                    // TODO: Send close or something
+                    return;
+                }
+                Err(err) => {
+                    eprintln!("WS Error: {}", err);
+                    return;
+                }
+            };
 
-    Ok(res)
+            if let Err(err) = sender.send(msg).await {
+                eprintln!("Error sending msg: {}", err);
+            }
+        }
+    });
+
+    Ok(())
+}
+
+pub struct WebSocketHandler<H> {
+    handler: Option<H>,
+}
+
+impl<H, F> WebSocketHandler<H>
+where
+    H: FnMut(WebSocket) -> F + Send + 'static,
+    F: Future + Send,
+{
+    pub fn new(handler: H) -> Self {
+        Self {
+            handler: Some(handler),
+        }
+    }
+}
+
+impl<H, F> Service<Request<Body>> for WebSocketHandler<H>
+where
+    H: FnMut(WebSocket) -> F + Send + 'static,
+    F: Future + Send,
+{
+    type Response = Response<Body>;
+    type Error = Infallible;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+
+    fn poll_ready(
+        &mut self,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, req: Request<Body>) -> Self::Future {
+        let handler = self.handler.take();
+
+        let (client_sender, client_receiver) = tokio::sync::mpsc::channel(10);
+        let (server_sender, server_receiver) = tokio::sync::mpsc::channel(10);
+
+        let ws = WebSocket {
+            sender: client_sender,
+            receiver: server_receiver,
+        };
+
+        if let Some(mut handler) = handler {
+            tokio::task::spawn(async move {
+                (handler)(ws).await;
+            });
+        }
+
+        let fut = async {
+            if let Err(e) = handshake(&req) {
+                eprintln!("Handshake error: {}", e);
+                return Ok(bad_request());
+            }
+
+            let accept_key = match compute_accept_key(&req) {
+                Ok(key) => key,
+                Err(e) => {
+                    eprintln!("Computing Sec-WebSocket-Accept: {}", e);
+                    return Ok(bad_request());
+                }
+            };
+
+            println!("Lookin good!");
+            tokio::task::spawn(async move {
+                match req.into_body().on_upgrade().await {
+                    Ok(upgraded) => {
+                        if let Err(err) =
+                            handle_upgraded(upgraded, client_receiver, server_sender).await
+                        {
+                            eprintln!("WebSocket Error: {}", err);
+                        }
+                    }
+                    Err(err) => {
+                        eprintln!("Failed to upgrade: {}", err);
+                    }
+                }
+            });
+
+            let res = Response::builder()
+                .status(StatusCode::SWITCHING_PROTOCOLS)
+                .header(UPGRADE, "websocket")
+                .header(CONNECTION, "Upgrade")
+                .header("Sec-WebSocket-Accept", accept_key)
+                .body(Body::empty())
+                .expect("failed to build response");
+
+            Ok(res)
+        };
+
+        Box::pin(fut)
+    }
 }
 
 fn compute_accept_key(req: &Request<Body>) -> Result<String> {
@@ -267,7 +534,6 @@ fn handshake(req: &Request<Body>) -> Result<(), WsError> {
     };
 
     if ws_key_bytes.len() != 16 {
-        dbg!(&ws_key_bytes);
         return Err(WsError::ProtocolError(
             "Sec-WebSocket-Key decoded is not 16 bytes".into(),
         ));
